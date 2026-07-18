4619eb03624485b068741fe7053048ac62caba2a
Sparsh-I <sparsh.poonacha@gmail.com>
2026-07-18 01:42:01 -0700

    Reorganised boarding pass layout

--- trip-itinerary/src/components/UpcomingTripWidget.tsx
+++ trip-itinerary/src/components/UpcomingTripWidget.tsx
@@ -6,13 +6,20 @@ export default function UpcomingTripWidget() {
             <table>
                 <tbody>
                     <tr>
-                        <td style={{ verticalAlign: "middle",  padding: "8px 20px", borderRight: "1px dashed black" }}>
+                        <td>
+                            <h3>Trip Title</h3>
+                        </td>
+                        <td>
+                            <h3>LOCA➜TION</h3>
+                        </td>
+                        <td rowSpan={2} style={{ verticalAlign: "middle",  padding: "8px 40px", borderLeft: "1px dashed black" }}>
                             <h3 className="black-text">MMM</h3>
                             <h1 style={{margin: "-5px 0"}}>DD</h1>
                             <h3 className="black-text">YYYY</h3>
                         </td>
-                        <td>
-                            <h3 id="upcoming-trip-title">Trip Title</h3>
+                    </tr>
+                    <tr>
+                        <td colSpan={2}>
                             <p id="upcoming-trip-desc">This would be where the description of the trip would go, but not sure where this would be pulled from?</p>
                         </td>
                     </tr>
