b62a81174100b68db6b85c997792372a265cf8e1
Sparsh-I <sparsh.poonacha@gmail.com>
2026-07-17 21:55:10 -0700

    Added placeholder text to boarding pass

--- trip-itinerary/src/components/UpcomingTripWidget.tsx
+++ trip-itinerary/src/components/UpcomingTripWidget.tsx
@@ -11,6 +11,10 @@ export default function UpcomingTripWidget() {
                             <h1 style={{margin: "-5px 0"}}>DD</h1>
                             <h3 className="black-text">YYYY</h3>
                         </td>
+                        <td>
+                            <h3 id="upcoming-trip-title">Trip Title</h3>
+                            <p id="upcoming-trip-desc">This would be where the description of the trip would go, but not sure where this would be pulled from?</p>
+                        </td>
                     </tr>
                 </tbody>
             </table>
