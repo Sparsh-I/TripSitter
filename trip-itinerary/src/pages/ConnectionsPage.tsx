c8d27164089da233f7ec455fd4af082489b0e33d
Sparsh-I <sparsh.poonacha@gmail.com>
2026-07-24 19:31:55 -0700

    Added construction message on connections page

--- trip-itinerary/src/pages/ConnectionsPage.tsx
+++ trip-itinerary/src/pages/ConnectionsPage.tsx
@@ -1,9 +1,16 @@
 import NavBar from '../components/global/NavBar.tsx';
+import construction from '../assets/under-construction.png';
 
 export default function ConnectionsPage() {
     return (
         <div>
             <NavBar/>
+            <div style={{display: "flex", justifyContent: "center", alignItems: "center", paddingTop: "10%"}}>
+                <img src={construction} alt="Under construction" style={{ maxWidth: "150px" }} />
+            </div>
+            <div>
+                <h1>This page is still under construction.</h1>
+            </div>
         </div>
     );
 }
