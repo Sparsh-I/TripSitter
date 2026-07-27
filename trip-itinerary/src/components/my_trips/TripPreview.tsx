591d98ff5fb93fac4e71f54a9a8e24fd089f56f8
Sparsh-I <sparsh.poonacha@gmail.com>
2026-07-27 11:03:36 -0700

    Added and connected button to delete a trip

--- trip-itinerary/src/components/my_trips/TripPreview.tsx
+++ trip-itinerary/src/components/my_trips/TripPreview.tsx
@@ -4,12 +4,15 @@ import eye from '../../assets/trip_preview/eye.svg';
 import eyeHover from '../../assets/trip_preview/eye-hover.svg';
 import edit from '../../assets/trip_preview/edit.svg';
 import editHover from '../../assets/trip_preview/edit-hover.svg';
-import share from '../../assets/trip_preview/share.svg';
-import shareHover from '../../assets/trip_preview/share-hover.svg';
+// import share from '../../assets/trip_preview/share.svg';
+// import shareHover from '../../assets/trip_preview/share-hover.svg';
+import trash from '../../assets/trip_preview/trash.svg';
+import trashHover from '../../assets/trip_preview/trash-hover.svg';
 import { useNavigate } from "react-router-dom";
 import type { Trip } from "../../types/Trip.ts";
 import PreviewTripPopup from "./PreviewTripPopup.tsx";
 import {useState} from "react";
+import {getTrips, saveTrips} from "../../utils/TripStorage.ts";
 
 interface TripPreviewProps {
     tripDetails: Trip
@@ -25,6 +28,17 @@ export default function TripPreview({ tripDetails }: TripPreviewProps ) {
 
     function closePopup() {
         setShowPopup(false);
+    }
+
+    function deleteTrip(): void {
+        const trips = getTrips();
+        if (confirm("Are you sure you want to delete this trip?")) {
+            const newTrips = trips.filter(trip => trip.id !== tripDetails.id);
+            saveTrips(newTrips);
+        } else {
+            return;
+        }
+        location.reload();
     }
 
     return (
@@ -45,10 +59,10 @@ export default function TripPreview({ tripDetails }: TripPreviewProps ) {
                                 <img className="img-hover" src={editHover} alt="Edit Hovered"/>
                             </div>
                         </button>
-                        <button className="alt">
+                        <button className="alt delete" onClick={deleteTrip}>
                             <div className="image-container">
-                                <img className="img-main" src={share} alt="Share"/>
-                                <img className="img-hover" src={shareHover} alt="Share Hovered"/>
+                                <img className="img-main" src={trash} alt="Delete"/>
+                                <img className="img-hover" src={trashHover} alt="Delete Hovered"/>
                             </div>
                         </button>
                     </div>
